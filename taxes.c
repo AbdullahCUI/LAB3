a39566fff936bed9ae073cedd0a4d3df88fefcc7
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-21 16:13:05 +0500

    solved three different condition for agi tax

--- taxes.c
+++ taxes.c
@@ -27,25 +27,63 @@ int main(int argc, char **argv) {
     printf("How many children do you have? ");
     scanf("%d", &numChildren);
   }
-{
+
   //TODO: compute the tax, child credit, and total tax here
- 
+  if(agi>=0 && agi <=19900){
+
+   childCredit=2000;
+   childCredit=numChildren;
+
+   tax=(numChildren/100)*totalTax;
+    totalTax=agi*0;
   printf("AGI:          $%10.2f\n", agi);
   printf("Tax:          $%10.2f\n", tax);
   printf("Child Credit: $%10.2f\n", childCredit);
   printf("Total Tax:    $%10.2f\n", totalTax);
-}
- if (agi>=19901 && agi <=81050){
-   if(numChildren==0){
-   totalTax=(agi/100) *12.2;
+    
+  
+  
+  } else if (agi>=19901 && agi <=81050){
+    // childCredit=2000;
+  
+  childCredit=numChildren*2000;
+  tax=agi*(12.0/100);
+  totalTax=1990 + tax *(agi-19900)-childCredit;
+  totalTax=totalTax-childCredit;
+   
   printf("AGI:          $%12.2f\n", agi);
   printf("Tax:          $%12.2f\n", tax);
   printf("Child Credit: $12.2f\n", childCredit);
   printf("Total tax:    $%12.2f\n",totalTax);
- }
-  return 0;
- }
- printf("\n", numChildren);
+ }else if(agi>=81051 && agi<=172,750){
+  
+   childCredit=2000;
+   tax=agi *(22.0/100);
+   totalTax=(9328+tax) *(agi-81050) -childCredit;
+   tax=tax-childCredit;
+   childCredit=numChildren*2000;
+  //  tax=tax-childCredit;
+ {
+   
+    printf("AGI:          $%22.2f\n", agi);
+  printf("Tax:          $%22.2f\n", tax);
+  printf("Child Credit: $22.2\n", childCredit);
+  printf("Total tax:    $%22.2f\n",totalTax);
 
- else if(agi>=81051 || agi<=172,750);
+ }
+ 
+ 
+ 
 }
+
+ 
+}
+  
+  
+ 
+ 
+ 
+ 
+
+
+ 
