5664eeb5a1b8e7983849b4a534a74d1a1dbac741
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-21 18:59:43 +0500

    add to condition statment

--- taxes.c
+++ taxes.c
@@ -12,12 +12,38 @@
 
   double agi;
 
-  float chilCridet;
+  float childCridet;
  int numOfchilderns;
 
  float tax;
 
  float totaltax;
+
+  printf("Please Enter Agi-->");
+
+  scanf("%lf", &agi);
+
+  //remove newline
+
+  getchar ();
+
+  printf("Did You have Any children:( Y/N-->)");
+
+  c=getchar();
+
+  if (c=='Y' || c=='y'){
+    printf("How many children=>");
+
+    scanf("%d", &numOfchilderns);
+  }
+  if (agi>=0 && agi <=19900){
+
+    crildCridet=200;
+    tax=agi*0.10;
+  } else if (agi>=19901 && agi <=81050){
+    
+    tax=1990+0.12 *(agi-19900);
+  }
 
 }
  
