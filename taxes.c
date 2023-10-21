367f987fa0e6bf75c3124c7e3761cd35608875bb
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-21 13:25:49 +0500

    first tax case complete

--- taxes.c
+++ taxes.c
@@ -27,13 +27,25 @@ int main(int argc, char **argv) {
     printf("How many children do you have? ");
     scanf("%d", &numChildren);
   }
-
+{
   //TODO: compute the tax, child credit, and total tax here
-
+ 
   printf("AGI:          $%10.2f\n", agi);
   printf("Tax:          $%10.2f\n", tax);
   printf("Child Credit: $%10.2f\n", childCredit);
   printf("Total Tax:    $%10.2f\n", totalTax);
-
+}
+ if (agi>=19901 && agi <=81050){
+   if(numChildren==0){
+   totalTax=(agi/100) *12.2;
+  printf("AGI:          $%12.2f\n", agi);
+  printf("Tax:          $%12.2f\n", tax);
+  printf("Child Credit: $12.2f\n", childCredit);
+  printf("Total tax:    $%12.2f\n",totalTax);
+ }
   return 0;
+ }
+ printf("\n", numChildren);
+
+ else if(agi>=81051 || agi<=172,750);
 }
